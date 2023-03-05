0dd36ff327305d3f2e5b27209d63b71e416853fd
AbdulMalek <M_2008_HA@YAHOO.COM>
2023-03-04 20:25:03 -0500

    Created a page for viewing profiles

--- usport/src/App.js
+++ usport/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import MyProfile from './components/MyProfile';
 
 function App() {
   return (
     <div className="App">
-      {/* Add your components here */}
+      <MyProfile></MyProfile>
     </div>
   );
 }
